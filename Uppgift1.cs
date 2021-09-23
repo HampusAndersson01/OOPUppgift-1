5fb8e1b411db9711c53fca18430e3a85d173d9ae
Hampus Andersson <hampe.01@hotmail.com>
2021-09-23 12:37:51 +0200

    Uppgift 1

---
+++ Uppgift1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OOPUppgift_1
+{
+    class Person{
+        public string Firstname;
+        public string Lastname;
+        public int Age;
+        public string Summary(){
+            return Firstname + " " + Lastname + " är " + Age + " år gammal.";
+        }
+    }
+    class Uppgift1{
+        static void Main(string[] args){
+            Console.Write("Firstname:");
+            string Firstname = Console.ReadLine();
+            Console.Write("Lastname:");
+            string Lastname = Console.ReadLine();
+            Console.Write("Age:");
+            int Age = Convert.ToInt32(Console.ReadLine());
+
+            Person p = new Person{
+                Firstname = Firstname,
+                Lastname = Lastname,
+                Age = Age
+            };
+            Console.WriteLine(p.Summary());
+        }
+
+    }
+}
